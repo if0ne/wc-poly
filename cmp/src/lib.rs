9c07d540ad50b81f596004c387c9b22dbf9b702c
if0ne <pavel.agafonov.al@gmail.com>
2025-06-27 14:03:46 +0300

    changes: component moved from http to nats

--- cmp/src/lib.rs
+++ cmp/src/lib.rs
@@ -1,34 +1,57 @@
 mod bindings {
     use super::Component;
 
-    wit_bindgen::generate!({
-        with: {
-            "wasi:http/incoming-handler@0.2.2": wasmcloud_component::wasi::exports::http::incoming_handler,
-        },
-        generate_all
-    });
+    wit_bindgen::generate!({ generate_all });
 
-    wasmcloud_component::http::export!(Component);
+    export!(Component);
 }
-mod api;
-mod utils;
 
-use wasmcloud_component::http;
-
-use crate::utils::make_empty_response;
+use crate::bindings::{
+    exports::wasmcloud::messaging::handler::Guest,
+    wasmcloud::messaging::{
+        consumer,
+        types::{self, BrokerMessage},
+    },
+    yasp::llm::ollama,
+};
 
 struct Component;
 
-impl http::Server for Component {
-    fn handle(
-        request: http::IncomingRequest,
-    ) -> http::Result<http::Response<impl http::OutgoingBody>> {
-        match request.uri().path() {
-            "/api/chat" => api::chat(request),
-            "/api/list" => api::list(request),
-            "/api/pull" => api::pull(request),
-            "/api/delete" => api::delete(request),
-            _ => make_empty_response(http::StatusCode::NOT_FOUND),
+impl Guest for Component {
+    fn handle_message(msg: BrokerMessage) -> Result<(), String> {
+        let result = match msg.subject.split('.').skip(3).next().unwrap() {
+            "chat" => {
+                let body = String::from_utf8_lossy(&msg.body);
+                ollama::chat(&body)
+            }
+            "list" => ollama::model_list(),
+            "pull" => {
+                let body = String::from_utf8_lossy(&msg.body);
+                ollama::pull(&body)
+            }
+            "delete" => {
+                let body = String::from_utf8_lossy(&msg.body);
+                ollama::delete(&body)
+            }
+            _ => Err("unknown api".to_string()),
+        };
+
+        if let Some(reply) = msg.reply_to {
+            let body = match result {
+                Ok(body) => body.into_bytes(),
+                Err(err) => serde_json::to_vec(&serde_json::json!({
+                    "error": err
+                }))
+                .unwrap(),
+            };
+
+            let _ = consumer::publish(&types::BrokerMessage {
+                subject: reply,
+                reply_to: None,
+                body,
+            });
         }
+
+        Ok(())
     }
 }
